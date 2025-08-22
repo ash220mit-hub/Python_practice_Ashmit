fa37f623cb15672ff6d5c24b2adb57971ed84be4
ash220mit-hub <ash220mit@gmail.com>
2025-08-22 10:34:07 +0530

    Add files via upload
    Fabonacci

---
+++ Fibonacci series.py
@@ -0,0 +1,18 @@
+num=0
+num1=1
+i=1
+print ('\t\t(Fibonacci series)')
+print (num,end=',')
+print (num1,end=',')
+while i<=10:
+    result=num+num1
+    print(result,end=',')
+    num=num1
+    num1=result
+    i+=1
+    
+    
+    
+    
+    
+    
