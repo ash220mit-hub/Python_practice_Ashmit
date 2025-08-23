66801c70f242badf2e8d74268d2f13f5f5fa1574
ash220mit-hub <ash220mit@gmail.com>
2025-08-23 17:09:00 +0530

    Add files via upload
    lambda

---
+++ lambda.py
@@ -0,0 +1,7 @@
+friend_name=['Sunny','Ashmit','Buddhan','Tuntun']
+city_name=['Bihar','punjab','Haryana','Rajasthan','Himachal']
+print_len=lambda list :len(list)
+print (print_len(city_name))
+
+
+
