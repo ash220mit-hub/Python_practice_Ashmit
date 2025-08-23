b6ef8590f0eab390fafc610461d85e287c839e93
ash220mit-hub <ash220mit@gmail.com>
2025-08-23 16:48:33 +0530

    Add files via upload
    Findlenusing_func

---
+++ findlen.py
@@ -0,0 +1,7 @@
+friend_name=['Sunny','Ashmit','Buddhan','Tuntun']
+city_name=['Bihar','punjab','Haryana','Rajasthan','Himachal']
+def print_len(list):
+    print (len(list))
+print_len(friend_name)
+print_len(city_name)
+
