35e09b24fd63a9ef1caf1be2238fbaf7cddffe78
ash220mit-hub <ash220mit@gmail.com>
2025-08-25 15:22:42 +0530

    Add files via upload
    Recursion topic

---
+++ sum of n no.using recursion.py
@@ -0,0 +1,14 @@
+def cal_sum(n):
+    if n>=1:
+        if n==1:
+            return 1
+        else:
+            return n + cal_sum(n-1) 
+print(cal_sum(10))  
+                
+                
+                
+                
+                
+                
+            
