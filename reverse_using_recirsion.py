268ffea87357a694c5bcc6b62a1345d09b1999f6
ash220mit-hub <ash220mit@gmail.com>
2025-08-27 17:19:25 +0530

    Add files via upload
    Reverse using recursion

---
+++ reverse_using_recirsion.py
@@ -0,0 +1,12 @@
+List=[ ]
+def reverse(n):
+    if n==0:
+        return 
+    else:
+        l=n%10
+        List.append(l)
+        return reverse(n//10)
+num=int(input('\t\tenter any number:'))
+reverse(num)
+num1=int(''.join(map(str,List)))
+print (num1)
